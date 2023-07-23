33221816bbf9e6fa007851e1f5131ced6917703a
devabormon <dev.abormon28@gmail.com>
2023-07-23 11:45:40 +0600

    [DONE] Boiler Plate

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+module.exports = {
+    root: true,
+
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['dist', 'node_modules'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'no-unused-vars': 'error',
+        'no-undef': 'error',
+        'no-unused-expressions': 'error',
+        'no-unreachable': 'error',
+        'no-console': 'warn',
+    },
+};
